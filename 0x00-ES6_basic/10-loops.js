941dc73c8a111ea045db3d19e5f899ba0ae8bb6f
Muhammadmansur <muhammadmansur737@gmail.com>
2024-09-30 22:21:19 +0000

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const nArray = [];
+  for (const value of array) {
+    nArray.push(appendString + value);
+  }
+
+  return array;
+}
