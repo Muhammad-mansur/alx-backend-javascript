f74c17487af694c23a59a0673bc37128c1db1dc5
Muhammadmansur <muhammadmansur737@gmail.com>
2024-10-20 08:22:00 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,8 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([result1, result2]) => {
       console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
-    }).catch(() => { console.log('Signup system offline'); });
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
