11408c4dea2a152f99406b130ae5995493cb764d
Muhammadmansur <muhammadmansur737@gmail.com>
2024-10-21 20:06:52 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,15 @@
 import signUpUser from './4-user-promise';
-import uploadPhoto from './photo-reject';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  
+export default function handleProfileSignup(firstName, lastName, fileName)   
+ {
+  return Promise.all([
+    signUpUser(firstName, lastName)   
+
+      .then((result) => ({ status: 'fulfilled', value: result }))
+      .catch((error) => ({ status: 'rejected', value: error })),
+    uploadPhoto(fileName)
+      .then((result) => ({ status: 'fulfilled', value: result }))
+      .catch((error) => ({ status: 'rejected', value: error })),
+  ]);
 }
