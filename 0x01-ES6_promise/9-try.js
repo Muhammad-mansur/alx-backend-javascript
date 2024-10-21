c0d041752940f3f63761b319731ba63bbb1133c6
Muhammadmansur <muhammadmansur737@gmail.com>
2024-10-21 20:00:41 +0000

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,9 +3,9 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-      queue.push(`Error: ${err.message}`);
+    queue.push(`Error: ${err.message}`);
   } finally {
-      queue.push('Guardrail was processed');
+    queue.push('Guardrail was processed');
   }
   return queue;
 }
